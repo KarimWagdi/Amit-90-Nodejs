131e1679cb6a0b56a8312411650893cacc48ace1
KarimWagdi <karim.wagdi@matnsolutions.com>
2025-01-23 23:02:33 +0200

    data

---
+++ js/lec_2/index.js
@@ -0,0 +1,75 @@
+// const x = 100
+// // console.log(x % 3);
+// const y = x % 7
+
+// console.log(y);
+
+// if(x < 10){
+//     console.log("data");
+// }else if(x > 10){
+//     console.log("data_1");
+// }else{
+//     console.log("data_2");
+// }
+
+// const x = prompt("enter number_1")
+
+// const y = prompt("enter number_2")
+
+// const firstValue = (+x + +y) % 3
+
+// const secondValue = (+x - +y) % 3
+
+// if(firstValue> 0 && secondValue >0 ){
+//     console.log('both values are odd');
+// }else if(firstValue == 0 && secondValue == 0){
+//     console.log('both values are even');
+// }else{
+//     if(firstValue == 0 ){
+//         console.log('only concat value is even');
+//     }else{
+//         console.log('only subtraction value is even');
+//     }
+// }
+
+console.log(new Date().getDay());
+
+const x = prompt()
+let day;
+switch (+x) {
+  case 0:
+    day = "Sunday";
+    console.log(day);
+    break;
+  case 1:
+    day = "Monday";
+    console.log(day);
+    break;
+  case 2:
+    day = "Tuesday";
+    console.log(day);
+    break;
+  case 3:
+    day = "Wednesday";
+    console.log(day);
+    break;
+  case 4:
+    day = "Thursday";
+    console.log(day);
+  case 5:
+    day = "Friday";
+    console.log(day);
+    break;
+  case  6:
+    day = "Saturday";
+    console.log(day);
+    break;
+}
+
+
+
+
+
+
+
+
