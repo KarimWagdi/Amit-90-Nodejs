99f126e8b283daf8f2655164b575250472808ebe
KarimWagdi <karim.wagdi@matnsolutions.com>
2025-02-11 22:24:25 +0200

    Data

---
+++ js/lec_7/index.js
@@ -0,0 +1,34 @@
+// 'use strict';
+
+// // let name = 'karim'
+
+// // console.log(`my name is ${names}`);
+
+// const arr = ['name','name','name','name','name','name','name','name','name','wagdi','karim']
+
+// // const data = new Set(arr);
+
+// // console.log(data);
+
+// let data = new Map();
+// data.set('name','wagdi')
+// data.set('age',30)
+// data.set('city','karachi')
+// data.set('startWork', 8)
+// data.set('endWork', 24 )
+// data.set(true ,'he is in wotrk')
+// data.set(false ,"he isn't in work")
+
+// data.set([12,23123,44,5,4,3,122], 'asdasdasdasdasdasdas')
+
+// let hour = 12 
+// console.log(data.get(true) , data.get(false) , data.get("city"));
+
+// console.log(data);
+
+console.log('now');
+const data = setTimeout(() =>{
+console.log('after 6 seconds');
+}, 6000);
+
+
